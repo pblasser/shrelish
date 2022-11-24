1f73494d3e34165868c3ce11bfcdf3cb8a52db2d
U-DESKTOP-SG4C4O2\HP <pete@ciat-lonbarde.net>
2022-11-24 10:29:39 +0100

    switchterrace

--- BenjoTop.rb
+++ BenjoTop.rb
@@ -17,32 +17,57 @@ class BenjoTop < PlumPott
   return @curxo + mm(x) 
  end
  def dy(y)
-  return @stary + mm(-y)
+  #return @stary + mm(-y)
+  return @stary + mm(y)
  end
 
  def KYCON5P(x,y,r)
-  shallot(dx(y),dy(x),1)
+  
+  #shallot(dx(y),dy(x),1)
  end
  def KYCONMINIJACK(x,y,r)
-  shallot(dx(y),dy(x),-1)
+  #shallot(dx(y),dy(x),-1)
  end
  def KOBICONDC(x,y,r)
-  shallot(dx(y),dy(x),-1)
+  #shallot(dx(y),dy(x),-1)
  end
  def JOHNSON(x,y,r)
   johnson(dx(y),dy(x))
  end
  def balpspot(x,y,r) 
+  r=-r
   alpspot(dx(y),dy(x),180+r)
  end
  def screw(x,y,r)
   looscrew(dx(y),dy(x))
  end
+ def swat(x,y,r)
+  r=-r
+  rotabox(dx(y),dy(x),mm(11),mm(7),r)
+  gotoPoint(dx(y),dy(x))
+  penetrate(mm(-2.5))
+  rotaframe(dx(y),dy(x),mm(14),mm(10),r)
+  penetrate(mm(-5))
+  rotaframe(dx(y),dy(x),mm(14),mm(10),r)
+    penetrate(mm(-7.5))
+  rotaframe(dx(y),dy(x),mm(14),mm(10),r)
+
+  penetrate(mm(-2.5))
+  rotaframe(dx(y),dy(x),mm(17),mm(13),r)
+    penetrate(mm(-5))
+  rotaframe(dx(y),dy(x),mm(17),mm(13),r)
+
+  penetrate(mm(-2.5))
+  rotaframe(dx(y),dy(x),mm(20),mm(16),r)
+
+  retract
+ end 
  def boxo()
   @curxo += MARJ
   @curxo += $halfwidth
   KYCON5P(0,0,180)
 balpspot(30,20,135)
+swat(33.3333-50,33.3333,-45)
 screw(44,6,0)
 JOHNSON(44,30,0)
 balpspot(0,70,135)
